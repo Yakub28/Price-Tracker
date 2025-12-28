7e3eece89e5879696d22e176a79c9eca6a24c9d7
Yakub Yakubov <yakubyakubov2003@mail.ru>
2025-12-28 21:20:09 +0100

    feat:config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+type Config struct {
+	APIKey         string
+	UpdateInterval time.Duration
+	AlertThreshold float64
+	DefaultSymbols []string
+}
+
+func Load() (*Config, error) {
+	apiKey := os.Getenv("ALPHA_VANTAGE_API_KEY")
+	if apiKey == "" {
+		return nil, fmt.Errorf("ALPHA_VANTAGE_API_KEY environment variable not set")
+	}
+
+	return &Config{
+		APIKey:         apiKey,
+		UpdateInterval: 5 * time.Minute,
+		AlertThreshold: 5.0,
+		DefaultSymbols: []string{"AAPL", "GOOGL", "MSFT", "TSLA"},
+	}, nil
+}
